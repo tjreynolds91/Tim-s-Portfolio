b47415ed3133b669b1b09b1ae4883e856f33d75d
tjreynolds91 <tjreynolds91@gmail.com>
2021-03-11 10:41:14 -0500

    hamburger colaps/expand

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   
+    /* Mobile nav*/
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon');
+
+        nav.slideToggle(200);
+        if (icon.hasName('menu-outline')) {
+            icon.addName('close-outline');
+            icon.removeName('menu-outline');
+        } else {
+            icon.addName('menu-outline');
+            icon.removeName('close-outline');
+        }
+    });
 });
